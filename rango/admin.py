a3771d982276550f0b6247599e2e0c45316ff426
alextonca <alextonca00@gmail.com>
2021-09-22 12:58:16 +0100

    test

--- rango/admin.py
+++ rango/admin.py
@@ -6,9 +6,8 @@ class PageAdmin(admin.ModelAdmin):
     list_display = ('title', 'category', 'url')
 
 class CategoryAdmin(admin.ModelAdmin):
-    prepopulated_fields = {'slug':('name', )}
+    prepopulated_fields = {'slug': ('name',)}
 
 admin.site.register(Category, CategoryAdmin)
 admin.site.register(Page, PageAdmin)
 admin.site.register(UserProfile)
-
--- rango/forms.py
+++ rango/forms.py
@@ -1,40 +1,35 @@
 from django import forms
-from rango.models import Page, Category
 from django.contrib.auth.models import User
-from rango.models import UserProfile
+from rango.models import Page, Category
+
+# We could add these forms to views.py, but it makes sense to split them off into their own file.
 
 class CategoryForm(forms.ModelForm):
-    name=forms.CharField(max_length=128, help_text="Please enter the category name.")
+    name = forms.CharField(max_length=Category.NAME_MAX_LENGTH, help_text="Please enter the category name.")
     views = forms.IntegerField(widget=forms.HiddenInput(), initial=0)
     likes = forms.IntegerField(widget=forms.HiddenInput(), initial=0)
-    slug=forms.CharField(widget=forms.HiddenInput(), required=False)
-    
+    slug = forms.CharField(widget=forms.HiddenInput(), required=False)
+
     class Meta:
         model = Category
-        fields = ('name', )
-        
+        fields = ('name',)
+
 class PageForm(forms.ModelForm):
-    title = forms.CharField(max_length=128, help_text="Please enter the title of the page.")
+    title = forms.CharField(max_length=Page.TITLE_MAX_LENGTH, help_text="Please enter the title of the page.")
     url = forms.URLField(max_length=200, help_text="Please enter the URL of the page.")
     views = forms.IntegerField(widget=forms.HiddenInput(), initial=0)
-    
+
     class Meta:
         model = Page
-        exclude = ('category', )
-        
-class UserForm(forms.ModelForm):
-    password = forms.CharField(widget=forms.PasswordInput())
+        exclude = ('category',)
     
-    class Meta:
-        model = User
-        fields = ('username', 'email', 'password',)
+    def clean(self):
+        cleaned_data = self.cleaned_data
+        url = cleaned_data.get('url')
+
+        if url and not url.startswith('http://'):
+            url = f'http://{url}'
+            cleaned_data['url'] = url
         
-class UserProfileForm(forms.ModelForm):
-    class Meta:
-        model = UserProfile
-        fields = ('website', 'picture',)
-        
-        
-    
-    
-    
+        return cleaned_data
+
